6715364b419fb15ab1e3c87c02928a09c8dc1206
med abbes <muhamed.abes@gmail.com>
2023-07-09 13:29:08 +0100

    2version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
-import './App.css';
-import { Navbar,Container,Nav,NavDropdown,Card,ListGroup,Placeholder,Form} from 'react-bootstrap';
+// import './App.css';
+import { Navbar,Container,Nav,NavDropdown,Card,Form} from 'react-bootstrap';
+import './style.css'
 function App() {
 
   return (
@@ -36,36 +37,69 @@ function App() {
 
 
 </div>
- <Card style={{ width: '18rem' }}>
- <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+
+<div className='carte' style={{display : "flex" , justifyContent: "space-around", margin:"50px" }} >
+
+<div className='carte1'>
+ <Card style={{ width: '26rem' }}>
+ <Card.Img variant="top" src="https://www.hydroelec-tn.com/wp-content/uploads/2022/03/Fonte-S630.png"style={{height:"500px",}} />
  <Card.Body>
-   <Card.Title>Card Title</Card.Title>
+   <Card.Title>Electropompe 30 cv</Card.Title>
    <Card.Text>
-     This is a card 
+     This is a submersible pompe
    </Card.Text>
  </Card.Body>
- <ListGroup className="list-group-flush">
-   <ListGroup.Item>Write your description</ListGroup.Item>
-   <ListGroup.Item>This is a good card</ListGroup.Item>
-   <ListGroup.Item>Magic card</ListGroup.Item>
- </ListGroup>
+ 
  <Card.Body>
    <Card.Link href="https://www.hydroelec-tn.com/wp-content/uploads/2022/03/Fonte-S630.png">Card Link</Card.Link>
    <Card.Link href="https://www.hydroelec-tn.com/wp-content/uploads/2022/03/Fonte-S630.png">Another Link</Card.Link>
  </Card.Body>
 </Card>
+</div>
+
+<div className='carte2'>
+<Card style={{ width: '26rem' }}>
+ <Card.Img variant="top" src="https://cptechmaroc.ma/wp-content/uploads/2020/12/4SR.jpg" style={{height:"500px"}} />
+ <Card.Body>
+   <Card.Title>Electropompe 15 cv</Card.Title>
+   <Card.Text>
+     This a submersible pompe
+   </Card.Text>
+ </Card.Body>
+ 
+ <Card.Body>
+   <Card.Link href="https://cptechmaroc.ma/wp-content/uploads/2020/12/4SR.jpg">Card Link</Card.Link>
+   <Card.Link href="https://cptechmaroc.ma/wp-content/uploads/2020/12/4SR.jpg">Another Link</Card.Link>
+ </Card.Body>
+</Card>
+</div>
+
+<div className='carte3'>
+<Card style={{ width: '26rem' }}>
+ <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3a1kd5wpBYJonqfD-U8P2ePYXTllklCOFyG8HB8_bGBdqVplNw_Jdsdnfq1xgCl7tXLY&usqp=CAU" style={{height:"500px"}}/>
+ <Card.Body>
+   <Card.Title>Electropompe 1cv</Card.Title>
+   <Card.Text>
+     This is a electropmpe immergeable
+   </Card.Text>
+ </Card.Body>
+ 
+ <Card.Body>
+   <Card.Link href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3a1kd5wpBYJonqfD-U8P2ePYXTllklCOFyG8HB8_bGBdqVplNw_Jdsdnfq1xgCl7tXLY&usqp=CAU">Card Link</Card.Link>
+   <Card.Link href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3a1kd5wpBYJonqfD-U8P2ePYXTllklCOFyG8HB8_bGBdqVplNw_Jdsdnfq1xgCl7tXLY&usqp=CAU">Another Link</Card.Link>
+ </Card.Body>
+</Card>
+</div>
+</div>
 
 
 
-    <Form>
+    <Form style={{marginLeft : "100px" , marginBottom :"150px" , backgroundColor :"beige"}}>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="name@example.com" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
+      
     </Form>
   
 </>
